95cbe5a28defd5574e0900c278e10cb2d728bd96
KK-Designs <71038229+KK-Designs@users.noreply.github.com>
2022-11-06 20:38:15 -0800

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,37 +1,35 @@
-import clock from "clock";
-import document from "document";
-import { preferences } from "user-settings";
-import * as util from "../common/utils";
-import * as settings from "./settings.js";
-import { me as appbit } from "appbit";
-import { today } from "user-activity";
+import clock from 'clock';
+import * as document from 'document';
+import { prefereces } from 'user-settings';
+import * as util from '../common/utils';
+import * as settings from './settings.js';
 
 // Update the clock every minute
-clock.granularity = "minutes";
+clock.granularity = 'minutes';
 
 // Get a handle on the <text> element
-const myLabel = document.getElementById("myLabel");
+const myLabel = document.getElementById('myLabel');
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
-  let today = evt.date;
-  let hours = today.getHours();
-  if (preferences.clockDisplay === "12h") {
-    // 12h format
-    hours = hours % 12 || 12;
-  } else {
-    // 24h format
-    hours = util.zeroPad(hours);
-  }
-  let mins = util.zeroPad(today.getMinutes());
-  myLabel.text = `${hours}:${mins}`;    
+	let today = evt.date;
+	let hours = today.getHours();
+	if (preferences.clockDisplay === '12h') {
+		// 12h format
+		hours = hours % 12 || 12;
+	} else {
+		// 24h format
+		hours = util.zeroPad(hours);
+	}
+	let mins = util.zeroPad(today.getMinutes());
+	myLabel.text = `${hours}:${mins}`;
 }
 
 // Start Settings
 settings.initialize();
 
 
-import { me } from "appbit";
+import { me } from 'appbit';
 
-console.log("Application ID: " + me.applicationId);
-console.log("Build ID: " + me.buildId);
+console.log('Application ID: ' + me.applicationId);
+console.log('Build ID: ' + me.buildId);
